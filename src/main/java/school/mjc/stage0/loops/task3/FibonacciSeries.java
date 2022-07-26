a116f24ea59db2f3c233898d04fed6bd24bfd30b
Nadzeya Petrova <nightmare.evans13@gmail.com>
2022-07-27 00:15:21 +0300

    For Loops

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int n1 = 0;
+        int n2 = 1;
+        System.out.println(n1 + "\n" + n2);
+        for (int i = 1; i < lastFibonacci-1; i++) {
+            System.out.println(i);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        int n = 9;
+        int sum = 0;
+        if (lengthOfLastNumber <= 0) {
+            System.out.println("0");
+        } else {
+            for (int i = 1; i <= lengthOfLastNumber; i++) {
+                sum += n;
+                n = n * 10 + 9;
+            }
 
+            System.out.println(sum);
+        }
     }
 }
